7e8cb2f5ef5f5f944d776a84f444d0e87f99df3b
Thinuj01 <thinujahettiarachchi@gmail.com>
2023-07-28 07:36:51 +0530

    javaFX & sqlDUMP included
    plz modify through sub branch

---
+++ LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/LoginController.java
@@ -0,0 +1,71 @@
+package com.example.librarymanagementsystem;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class LoginController {
+   @FXML
+    private TextField txtUserName;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private Label lblStatus;
+
+    @FXML
+    protected void onClickLogin(){
+        try {
+            String UserName = txtUserName.getText();
+            String Password = txtPassword.getText();
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms", "root", "Thinuja21033");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("select * from login");
+
+
+            while(resultSet.next()){
+                String x = resultSet.getString("UserName");
+                String y =resultSet.getString("Password");
+//                System.out.println(x);
+//                System.out.println(y);
+                if(UserName.equals(x) && Password.equals(y)){
+                    //lblStatus.setText("Login Success");
+                    String z =resultSet.getString("Role");
+                    if(z.equals("Admin")){
+
+                    }
+                }else{
+                    lblStatus.setText("Invalid UserName or Password");
+                }
+            }
+
+            statement.close();
+            connection.close();
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    protected void onClickRegister() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Register.fxml"));
+        Scene scene1 = new Scene(fxmlLoader.load(), 650, 800);
+        Stage stage1 = new Stage();
+        stage1.setTitle("Register");
+        stage1.setScene(scene1);
+        stage1.show();
+    }
+
+
+
+}
---
+++ LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/RegisterController.java
@@ -0,0 +1,77 @@
+package com.example.librarymanagementsystem;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.sql.*;
+
+public class RegisterController {
+    @FXML
+    private TextField txtFName;
+    @FXML
+    private TextField txtLName;
+    @FXML
+    private TextField txtBOD;
+    @FXML
+    private TextField txtUserName;
+    @FXML
+    private TextField txtPassword;
+    @FXML
+    private TextField txtRePassword;
+    @FXML
+    private ToggleGroup tgRole;
+    @FXML
+    private RadioButton rdbAdmin;
+    @FXML
+    private RadioButton rdbUser;
+    @FXML
+    private Label lblWarning;
+    @FXML
+    private AnchorPane RegisterPane;
+    @FXML
+    protected void onClickRegistered() throws SQLException {
+
+            String FirstName = txtFName.getText();
+            String LastName = txtLName.getText();
+            String BOD = txtBOD.getText();
+            String UserName = txtUserName.getText();
+            String Password = txtPassword.getText();
+            String RePassword = txtRePassword.getText();
+            String Role = "";
+            if (rdbAdmin.isSelected()) {
+                Role = "Admin";
+            } else {
+                Role = "User";
+            }
+            if (Password.equals(RePassword)) {
+
+                String sql = "INSERT INTO login" + "(FName,LName,BOD,UserName,Password,Role)" +
+                        "VALUES(\"" + FirstName + "\",\"" + LastName + "\",\"" + BOD + "\",\"" + UserName + "\",\"" + Password + "\",\"" + Role + "\")";
+                //System.out.println(sql);
+
+                Connection connection2 = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms", "root", "Thinuja21033");
+                Statement statement2 = connection2.createStatement();
+                statement2.execute(sql);
+
+                statement2.close();
+                connection2.close();
+                Stage stage;
+                stage =(Stage) RegisterPane.getScene().getWindow();
+                stage.close();
+            }
+
+            else{
+                lblWarning.setText("Passwords dosen't match");
+        }
+
+
+    }
+}
+
