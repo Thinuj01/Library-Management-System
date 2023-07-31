50aab7731cc7eb794aa599f005edb23699c3b7ed
kasun751 <kasunp751@gmail.com>
2023-07-31 18:19:45 +0530

    Add new dump file

---
+++ lmsNewdump.sql
@@ -0,0 +1,113 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: lms
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `book_details`
+--
+
+DROP TABLE IF EXISTS `book_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `book_details` (
+  `Book_ID` varchar(6) NOT NULL,
+  `Book_Name` varchar(45) NOT NULL,
+  `Author` varchar(45) NOT NULL,
+  `Category` varchar(45) NOT NULL,
+  `Price` varchar(45) DEFAULT NULL,
+  `NoOfPages` varchar(45) DEFAULT NULL,
+  `Location` varchar(45) NOT NULL,
+  PRIMARY KEY (`Book_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_details`
+--
+
+LOCK TABLES `book_details` WRITE;
+/*!40000 ALTER TABLE `book_details` DISABLE KEYS */;
+INSERT INTO `book_details` VALUES ('','','','','','',''),('A0002','SQL Programming','Thinuja','Programming','2000.00','200','P05'),('A028','Madol duwa','Martin Wikramasinhe','nowel','510.00','470','N09'),('A042','Martin Loother','John Smith','Characteristc','2100.00','265','C024');
+/*!40000 ALTER TABLE `book_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `login` (
+  `user_ID` int NOT NULL AUTO_INCREMENT,
+  `FName` varchar(45) NOT NULL,
+  `LName` varchar(45) NOT NULL,
+  `BOD` date DEFAULT NULL,
+  `UserName` varchar(45) NOT NULL,
+  `Password` varchar(45) NOT NULL,
+  `Role` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_ID`),
+  UNIQUE KEY `User_ID_UNIQUE` (`user_ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `login`
+--
+
+LOCK TABLES `login` WRITE;
+/*!40000 ALTER TABLE `login` DISABLE KEYS */;
+INSERT INTO `login` VALUES (1,'Thinuja','Hettiarachchi','2001-09-27','Thinuj','123','Admin'),(2,'Tiran','Hettiarachchi','1998-03-11','Tiran','456','Admin'),(3,'Kasun','Priyadharashana','2000-05-15','Kasun','789','Admin'),(4,'Ravindu','Dilshan','2000-08-14','Ravi','1234','Admin'),(5,'Manula','DDD','2000-02-11','Manu','852','Admin'),(6,'Maheesha','Nethmina','2000-03-16','Mahee','147','Admin'),(7,'pasidu','jayasinha','2000-08-10','pasidu','1234','User');
+/*!40000 ALTER TABLE `login` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_books`
+--
+
+DROP TABLE IF EXISTS `user_books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_books` (
+  `UserID` varchar(6) NOT NULL,
+  `BookID` varchar(6) NOT NULL,
+  `date_of_delivery` date DEFAULT NULL,
+  `date_of_recived` date DEFAULT NULL,
+  `amount_of_fine` decimal(5,1) DEFAULT NULL,
+  `status` varchar(10) DEFAULT 'pending',
+  PRIMARY KEY (`UserID`,`BookID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_books`
+--
+
+LOCK TABLES `user_books` WRITE;
+/*!40000 ALTER TABLE `user_books` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_books` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-31 18:18:47
