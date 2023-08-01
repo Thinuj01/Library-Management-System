88ec785ad4c7ec675ba9c6f594301c69068e2683
Thinuj01 <thinujahettiarachchi@gmail.com>
2023-08-01 09:49:41 +0530

    icons added

--- LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/HelloApplication.java
+++ LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/HelloApplication.java
@@ -9,7 +9,8 @@ import java.io.IOException;
 
 public class HelloApplication extends Application {
     protected static String DB_USERNAME = "root";
-    protected static String DB_PASSWORD = "123456789";
+    //protected static String DB_PASSWORD = "123456789";
+    protected static String DB_PASSWORD = "Thinuja21033";
     @Override
     public void start(Stage stage) throws IOException {
         LoadWindow.loadInterFace("Login.fxml","Hello!",1280, 800);
