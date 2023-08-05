67445c24acf4cd86357111f281cc91b76a7bded2
Thinuj01 <thinujahettiarachchi@gmail.com>
2023-08-05 07:25:14 +0530

    Update AdminInterfaceController.java

--- LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/AdminInterfaceController.java
+++ LibraryManagementSystem/src/main/java/com/example/librarymanagementsystem/AdminInterfaceController.java
@@ -233,7 +233,7 @@ public class AdminInterfaceController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         try {
-            System.out.println("Kasun");
+            System.out.println("Kasun x");
             lblProUser.setText(UserName);
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/lms", HelloApplication.DB_USERNAME, HelloApplication.DB_PASSWORD);
             data = FXCollections.observableArrayList();
