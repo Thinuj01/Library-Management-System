6134159fa93c51ec34a69d1a7c81a1bced7b43fe
kasun751 <kasunp751@gmail.com>
2023-08-16 08:10:59 +0530

    Revert "aa"
    This reverts commit 808ff91db40b3ca7025fd3c6ef0b48df70c841fb.

---
+++ LibraryManagementSystem/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.librarymanagementsystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires java.net.http;
+
+
+    opens com.example.librarymanagementsystem to javafx.fxml;
+    exports com.example.librarymanagementsystem;
+}
